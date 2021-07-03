3f21a4b99b467a6cea8ab46303238b12b7b1d2d8
Rahuk Kumar <rk7870382701@gmail.com>
2021-07-03 10:41:33 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const posts = [
+    {
+        id: 1,
+        title: "Backened Node JS developer",
+        company: "TCS Delhi",
+        city: "New Delhi",
+        description: "Node. js developers deploy and maintain network applications. They manage the exchange of data between servers and users, develop back-end components, and connect applications with other web services. They also support front-end developers by integrating web-based applications."
+    },
+
+    {
+        id: 2,
+        title: "Springboot developer",
+        company: "TCS Kolkata",
+        city: "Kolkata",
+        description: "The Java (Spring Boot) Developer would need to have at least 5+ years of experience. Requirements & Responsibilities: ... - Expertise developing enterprise-level web applications and RESTful APIs using MicroServices, with demonstrable production-scale experience."
+    },
+
+    {
+        id: 3,
+        title: "React JS front end developer",
+        company: "TCS Kolkata",
+        city: "Kolkata",
+        description: "React. js developers design and implement user interface components for JavaScript-based web and mobile applications using the React open-source library ecosystem. These skilled front-end developers are involved in all stages of interface component design, from conception through to final testing."
+    }
+];
+
+export default posts;
